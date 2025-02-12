98f57c690d012eadaaa347671736aeb2aed0921c
NicoNicoNico123 <zidnae852@hotmail.com>
2025-02-12 22:24:07 +0800

    12fev

--- threads/prefect/dailyflow.py
+++ threads/prefect/dailyflow.py
@@ -1,10 +1,11 @@
 import sys
 from pathlib import Path
+import random
 
 # Add the project root directory to sys.path
 sys.path.append(str(Path(__file__).parent.parent.parent))
 
-from threads import interact_chain, SerpAPI, Threads
+from threads.main import Main
 from prefect import flow, task
 from datetime import timedelta
 from prefect.client.schemas.schedules import IntervalSchedule
@@ -18,18 +19,17 @@ def chain(result):
     return interact_chain(result)
 
 @task
-def threads(output):
-    return Threads().main(output)
+def threads():
+    local_thread = Main.local_thread()
+    return local_thread
 
 @flow(log_prints=True)
 def daily_flow():
-    result = serp_api()
-    output = chain(result)
-    threads(output)
+    threads()
 
 if __name__ == "__main__":
     daily_flow.serve(
-        name="daily-flow-deployment",
-        interval=60 * 60 * 12,  # Runs every 12 hours
-        pause_on_shutdown=False  # Optional: prevents schedule from pausing on shutdown
+        name="yiying_threads_flow",
+        interval=timedelta(minutes=random.randint(30, 120)),
+        tags=["threads"]
     )
